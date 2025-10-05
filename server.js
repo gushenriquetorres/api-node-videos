7c570a1594b2794df749aa933c15f15202b99847
Gustavo Henrique <gustavohenrique@pactosolucoes.com.br>
2025-10-05 18:38:08 -0300

    Set host option listen

--- server.js
+++ server.js
@@ -44,4 +44,10 @@ server.delete('/videos/:id', async (request, reply) => {
   return reply.status(204).send()
 })
 
-server.listen({ port: process.env.PORT ?? 3333 }, () => console.log('Server running at http://localhost:3333'))
+server.listen(
+  {
+    host: '0.0.0.0',
+    port: process.env.PORT ?? 3333,
+  },
+  () => console.log('Server running at http://localhost:3333')
+)
